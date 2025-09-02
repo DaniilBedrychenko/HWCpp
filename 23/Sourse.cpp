473c5542c8ec8b0d092d25a46de07f689cee9dfc
DaniilBedrychenko <bedrychenko787@gmail.com>
2025-09-02 13:53:32 +0300

    Update Sourse.cpp

--- 23/Sourse.cpp
+++ 23/Sourse.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 class Fraction {
 private:
-    int num;
-    int den;
+    int numerator;
+    int denominator;
 
     int gcd(int a, int b) const {
         a = abs(a);
@@ -20,91 +20,91 @@ private:
     }
 
     void normalize() {
-        if (den < 0) {
-            num = -num;
-            den = -den;
+        if (denominator < 0) {
+            numerator = -numerator;
+            denominator = -denominator;
         }
-        if (num == 0) {
-            den = 1;
+        if (numerator == 0) {
+            denominator = 1;
         }
     }
 
 public:
-    Fraction() : num(0), den(1) {}
+    Fraction() : numerator(0), denominator(1) {}
 
-    Fraction(int n, int d) : num(n), den(d) {
+    Fraction(int n, int d) : numerator(n), denominator(d) {
         if (d == 0) {
             cerr << "Denominator cannot be zero! Setting to 1." << endl;
-            den = 1;
+            denominator = 1;
         }
         normalize();
     }
 
-    Fraction(const Fraction& other) : num(other.num), den(other.den) {}
+    Fraction(const Fraction& other) : numerator(other.numerator), denominator(other.denominator) {}
 
     void printFraction() const {
-        cout << num << "/" << den << endl;
+        cout << numerator << "/" << denominator << endl;
     }
 
     void printNumerator() const {
-        cout << num << endl;
+        cout << numerator << endl;
     }
 
     void printDenominator() const {
-        cout << den << endl;
+        cout << denominator << endl;
     }
 
     void addNumber(int number) const {
-        int new_num = num + number * den;
-        int new_den = den;
-        Fraction res(new_num, new_den);
-        cout << "Result: " << res.num << "/" << res.den << endl;
+        int newNumerator = numerator + number * denominator;
+        int newDenominator = denominator;
+        Fraction res(newNumerator, newDenominator);
+        cout << "Result: " << res.numerator << "/" << res.denominator << endl;
     }
 
     void addFraction(const Fraction& other) const {
-        int new_num = num * other.den + other.num * den;
-        int new_den = den * other.den;
-        Fraction res(new_num, new_den);
-        cout << "Result: " << res.num << "/" << res.den << endl;
+        int newNumerator = numerator * other.denominator + other.numerator * denominator;
+        int newDenominator = denominator * other.denominator;
+        Fraction res(newNumerator, newDenominator);
+        cout << "Result: " << res.numerator << "/" << res.denominator << endl;
     }
 
     void subtractNumber(int number) const {
-        int new_num = num - number * den;
-        int new_den = den;
-        Fraction res(new_num, new_den);
-        cout << "Result: " << res.num << "/" << res.den << endl;
+        int newNumerator = numerator - number * denominator;
+        int newDenominator = denominator;
+        Fraction res(newNumerator, newDenominator);
+        cout << "Result: " << res.numerator << "/" << res.denominator << endl;
     }
 
     void subtractFraction(const Fraction& other) const {
-        int new_num = num * other.den - other.num * den;
-        int new_den = den * other.den;
-        Fraction res(new_num, new_den);
-        cout << "Result: " << res.num << "/" << res.den << endl;
+        int newNumerator = numerator * other.denominator - other.numerator * denominator;
+        int newDenominator = denominator * other.denominator;
+        Fraction res(newNumerator, newDenominator);
+        cout << "Result: " << res.numerator << "/" << res.denominator << endl;
     }
 
     void multiplyNumber(int number) const {
-        int new_num = num * number;
-        int new_den = den;
-        Fraction res(new_num, new_den);
-        cout << "Result: " << res.num << "/" << res.den << endl;
+        int newNumerator = numerator * number;
+        int newDenominator = denominator;
+        Fraction res(newNumerator, newDenominator);
+        cout << "Result: " << res.numerator << "/" << res.denominator << endl;
     }
 
     void multiplyFraction(const Fraction& other) const {
-        int new_num = num * other.num;
-        int new_den = den * other.den;
-        Fraction res(new_num, new_den);
-        cout << "Result: " << res.num << "/" << res.den << endl;
+        int newNumerator = numerator * other.numerator;
+        int newDenominator = denominator * other.denominator;
+        Fraction res(newNumerator, newDenominator);
+        cout << "Result: " << res.numerator << "/" << res.denominator << endl;
     }
 
     void divideFraction(const Fraction& other) const {
-        if (other.num == 0) {
+        if (other.numerator == 0) {
             cout << "Division by zero!" << endl;
             return;
         }
-        int new_num = num * other.den;
-        int new_den = den * other.num;
-        Fraction res(new_num, new_den);
-        cout << "Result: " << res.num << "/" << res.den << endl;
+        int newNumerator = numerator * other.denominator;
+        int newDenominator = denominator * other.numerator;
+        Fraction res(newNumerator, newDenominator);
+        cout << "Result: " << res.numerator << "/" << res.denominator << endl;
     }
 
     void divideNumber(int number) const {
@@ -112,21 +112,21 @@ public:
             cout << "Division by zero!" << endl;
             return;
         }
-        int new_num = num;
-        int new_den = den * number;
-        Fraction res(new_num, new_den);
-        cout << "Result: " << res.num << "/" << res.den << endl;
+        int newNumerator = numerator;
+        int newDenominator = denominator * number;
+        Fraction res(newNumerator, newDenominator);
+        cout << "Result: " << res.numerator << "/" << res.denominator << endl;
     }
 
     void simplify() {
-        if (den == 0) return;
-        int g = gcd(num, den);
-        num /= g;
-        den /= g;
+        if (denominator == 0) return;
+        int g = gcd(numerator, denominator);
+        numerator /= g;
+        denominator /= g;
         normalize();
     }
 
     bool isProper() const {
-        return abs(num) < abs(den);
+        return abs(numerator) < abs(denominator);
     }
 };
